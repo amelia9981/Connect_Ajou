f1b5d0d706201754a59b0790176a1f94ccc19d38
Amy <amelia9981@ajou.ac.kr>
2021-04-13 13:13:21 +0900

    font

--- Pages/CommunityPage/GetInfo.js
+++ Pages/CommunityPage/GetInfo.js
@@ -1,21 +1,72 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { ScrollView, View, Text, StyleSheet, SectionList, TouchableOpacity, Alert } from 'react-native';
 import { Feather } from '@expo/vector-icons';
+import * as Font from 'expo-font';
 
 export default class getInfo extends Component {
+
+    constructor() {
+        super();
+
+        // 대량의 데이터
+        this.state = {
+            sectionDatas: [
+                // SectionList의 섹션 하나 객체에는 title, data 2개의 프로퍼티 필요
+                { title: 'Get Info', data: ["Official Announcement", "About Course", "Campus", "Random Question"] },
+                { title: 'Find Friend', data: ["Sports mate", "Finding Party Mates", "Finding Roommates"] },
+                { title: 'Food Yummy', data: ["Near Campus", "Vege Restaurant", "Halal Restaurant"] },
+                { title: 'Study Group', data: ["Languague Exchange", "Major Study", "Other Hobby"] },
+            ],
+        };
+    }
     render() {
         return (
-            <View style={style.container}>
-                <Text>HomeTab</Text>
-            </View>
+            <ScrollView style={style.root}>
+                {/* 리스트에 그룹칭이 가능하 리스트뷰 */}
+                {/* 3개의 필수 속성 */}
+                {/* 1) section - 섹션 title과 섹션별 data들을 가진 대량의 데이터*/}
+                {/* 2) renderSectionHeader - 섹션별 title영역에 그려질 컴포넌트를 리턴하는 콜백함수 지정 */}
+                {/* 3) renderItem - 섹션별 Item영역에 그려질 컴포넌트를 리턴하는 콜백함수 지정 */}
+                <SectionList
+                    sections={this.state.sectionDatas}
+                    renderSectionHeader={({ section }) => {
+                        return (
+                            <View style={style.sectionHeader}>
+                                <Text style={style.sectionTitle}>{section.title}</Text>
+                            </View>
+                        );
+                    }}
+                    renderItem={({ item, index, section }) => {
+                        return (
+                            <TouchableOpacity style={style.itemView} onPress={() => { this.clickItem(item) }} >
+                                <Text style={style.itemView}>{item}</Text>
+                            </TouchableOpacity>
+                        );
+                    }}
+                    // keyExtractor={(item,index)=>{return index}}
+                    keyExtractor={(item, index) => index}
+                >
+
+                </SectionList>
+            </ScrollView>
         );
+    }//render method..
+    clickItem = (item) => {
+        Alert.alert(item);
     }
 }
 
 const style = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    }
+    root: { flex: 1, padding: 16 },
+    sectionHeader: {
+        padding: 4,
+        backgroundColor: "#eeeeee",
+    },
+    sectionTitle: {
+        fontSize: 24,
+        fontWeight: 'bold',
+    },
+    itemView: {
+        padding: 8,
+    },
 });
