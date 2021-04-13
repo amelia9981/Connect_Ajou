49386443f969d05394701c05112eb26dc0832c81
Jieun Han <namu1092@ajou.ac.kr>
2021-04-13 13:59:53 +0900

    add loading page

--- App.js
+++ App.js
@@ -1,14 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator} from 'react-navigation-stack';
-import MainScreen from './MainScreenF';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import MainScreen from "./MainScreenF";
+import LoadingScreen from "./Pages/Loading";
 
-const AppStackNavigator = createStackNavigator({
-  Main: {
-    screen: MainScreen // MainScreen 컴포넌트를 네비게이터에 등록
+export default class extends React.Component {
+  state = {
+    isLoading: true,
+  };
+  componentDidMount = async () => {
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 3000);
+  };
+  render() {
+    const { isLoading } = this.state;
+    return isLoading ? <LoadingScreen /> : <MainScreen />;
   }
-});
+}
 
-export default createAppContainer(AppStackNavigator);
+// const AppStackNavigator = createStackNavigator({
+//   Main: {
+//     screen: MainScreen, // MainScreen 컴포넌트를 네비게이터에 등록
+//   },
+// });
+
+// export default createAppContainer(AppStackNavigator);
+
+// export default function App() {
+
+//}
