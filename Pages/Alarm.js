a0c1521920e798bec1f5d011280c2a03128b5f4a
Jieun Han <namu1092@ajou.ac.kr>
2021-04-20 13:20:54 +0900

    fix alarm page

--- Pages/Alarm.js
+++ Pages/Alarm.js
@@ -1,58 +1,47 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
-import { Feather } from "@expo/vector-icons";
-import { createAppContainer } from "react-navigation";
+import { Platform } from "react-native";
 import Notification from "./AlarmPage/Notification";
 import Message from "./AlarmPage/Message";
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 
 const Tab = createMaterialTopTabNavigator();
-class AlarmTab extends Component {
-  
-  render() {
-    return (
-      <Tab.Navigator tabBarOptions={{
-        animationEnabled: true,
-        swipeEnabled: true,
+
+function AlarmTab() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
         tabBarPosition: "top",
-          tabBarOptions: {
-            style: {
-              ...Platform.select({
-                ios: {
-                  backgroundColor: "white",
-                },
-                android: {
-                  backgroundColor: "white",
-                },
-              }),
+        swipeEnabled: false,
+      })}
+      tabBarOptions={{
+        style: {
+          ...Platform.select({
+            ios: {
+              backgroundColor: "white",
             },
-            activeTintColor: "#1E3D6B",
-            inactiveTintColor: "#D7DDE2",
-            upperCaseLabel: false,
-            showLabel: true,
-              labelStyle: {
-            marginTop: 40,
-            fontSize: 15,
-            fontFamily: "EBS훈민정음새론SB",
-          },
-          indicatorStyle: {
+            android: {
+              backgroundColor: "white",
+            },
+          }),
+        },
+        activeTintColor: "#1E3D6B",
+        inactiveTintColor: "#D7DDE2",
+        upperCaseLabel: false,
+        showLabel: true,
+        labelStyle: {
+          marginTop: 40,
+          fontSize: 15,
+          fontFamily: "EBS훈민정음새론SB",
+        },
+        indicatorStyle: {
           backgroundColor: "#1E3D6B",
         },
-      },
-    }}>
+      }}
+    >
       <Tab.Screen name="Notification" component={Notification} />
       <Tab.Screen name="Message" component={Message} />
     </Tab.Navigator>
-    )
-  }
+  );
 }
-
-const style = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
 
 export default AlarmTab;
